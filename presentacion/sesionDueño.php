1d537feb87c4cfaac516600b50628cce5f401d2a
danna <dannaa.molina.b@gmail.com>
2025-06-15 00:40:46 -0500

    Encabezados por usuario

--- presentacion/sesionDueño.php
+++ presentacion/sesionDueño.php
@@ -1,1 +1,13 @@
-Hola Dueño
+<?php
+if(session_status() === PHP_SESSION_NONE) session_start();
+
+if(!isset($_SESSION["rol"]) || $_SESSION["rol"] !== "dueño"){
+    header("Location: ?pid=" . base64_encode("presentacion/sesiones/noAutorizado.php"));
+    exit();
+}
+?>
+<body>
+<?php 
+include ("presentacion/encabezadoD.php");
+?>
+</body>
--- presentacion/sesionPaseador.php
+++ presentacion/sesionPaseador.php
@@ -1,1 +1,13 @@
-Hola paseador
+<?php
+if(session_status() === PHP_SESSION_NONE) session_start();
+
+if(!isset($_SESSION["rol"]) || $_SESSION["rol"] !== "paseador"){
+    header("Location: ?pid=" . base64_encode("presentacion/sesiones/noAutorizado.php"));
+    exit();
+}
+?>
+<body>
+<?php 
+include ("presentacion/encabezadoP.php");
+?>
+</body>
